ef3eff07a3e23dbc19e43f638875cd84d394e064
Simon Mourier <smourier@users.noreply.github.com>
2024-04-17 18:47:11 +0200

    wip

--- InteropBuilder.Cli/Program.cs
+++ InteropBuilder.Cli/Program.cs
@@ -9,7 +9,7 @@ namespace InteropBuilder.Cli
     {
         static void Main()
         {
-            Console.WriteLine("InteropBuilder - Copyright (C) 2017-" + DateTime.Now.Year + " Simon Mourier. All rights reserved.");
+            Console.WriteLine("Win32 Interop Builder - Copyright (C) 2017-" + DateTime.Now.Year + " Simon Mourier. All rights reserved.");
             Console.WriteLine();
             var configurationPath = CommandLine.Current.GetNullifiedArgument(0);
             if (CommandLine.Current.HelpRequested || configurationPath == null)
@@ -26,9 +26,7 @@ namespace InteropBuilder.Cli
             Console.WriteLine(Assembly.GetEntryAssembly()!.GetName().Name!.ToUpperInvariant() + " <config.json> [outputpath]");
             Console.WriteLine();
             Console.WriteLine("Description:");
-            Console.WriteLine("    This tool is used to generate .cs files from an interop builder assembly.");
-            Console.WriteLine();
-            Console.WriteLine("Options:");
+            Console.WriteLine("    This tool is used to generate Win32 interop .cs files from Microsoft.Windows.SDK.Win32Metadata and configuration directives.");
             Console.WriteLine();
             Console.WriteLine("Examples:");
             Console.WriteLine();
